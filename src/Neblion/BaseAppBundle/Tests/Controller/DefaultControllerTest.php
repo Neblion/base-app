076aacb6e3db67d360622bd12c904e7e7c6819a8
Thomas BIBARD <thomas.bibard@neblion.net>
2013-03-07 16:41:59 +0100

    Create first functional test, valid test env

--- src/Neblion/BaseAppBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Neblion/BaseAppBundle/Tests/Controller/DefaultControllerTest.php
@@ -2,12 +2,17 @@
 
 namespace Neblion\AppBundle\Tests\Controller;
 
+//use Neblion\BaseAppBundle\Tests\WebTestCase;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerTest extends WebTestCase
 {
     public function testIndex()
     {
-        
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $crawler = $client->followRedirect();
+        $this->assertTrue($crawler->filter('html:contains("Sign in")')->count() == 1);
     }
 }
---
+++ src/Neblion/BaseAppBundle/Tests/WebTestCase.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Neblion\BaseAppBundle\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
+
+abstract class WebTestCase extends BaseWebTestCase
+{
+    protected function login($username, $password)
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+        
+        $form = $crawler->selectButton('Login')->form();
+        $client->submit($form, array(
+            '_username'     => $username,
+            '_password'     => $password
+        ));
+
+        $this->assertEquals(302, $client->getResponse()->getStatusCode());
+        $client->followRedirect();
+        return $client;
+    }
+}
